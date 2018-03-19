f2ee7c30c333e45b8922078e92dfb76683815401
Aakash Thapa <thapa.aakash100@gmail.com>
2018-03-19 09:09:46 +0545

    Build and Added Static  Build Controls Component  to Builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import AuxWrapper from '../../hoc/AuxWrapper';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
   // constructor(props) {
@@ -12,10 +13,10 @@ class BurgerBuilder extends Component {
 
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 2,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
 
@@ -23,7 +24,7 @@ class BurgerBuilder extends Component {
     return (
       <AuxWrapper>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </AuxWrapper>
     );
   }
